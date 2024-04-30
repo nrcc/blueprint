2c0f432349e0f1945fd4504c5fe9018e37a8a537
cat <hhe@live.com>
2024-04-30 13:18:43 +0800

    blueprint init

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,31 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { NrccBluePrintPlugin } from './definitions';
+import type { BluetoothDevice, NrccBluePrintPlugin } from './definitions';
 
 export class NrccBluePrintWeb extends WebPlugin implements NrccBluePrintPlugin {
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
   }
+
+  connect(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  connectAndPrint(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  disconnect(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  list(): Promise<{ devices: BluetoothDevice[] }> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  print(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+  
 }
